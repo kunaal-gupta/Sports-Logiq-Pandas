f7fabf075b04ddbdd9a3d6818a1104da943ad54b
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2023-03-12 03:38:51 -0600

    added comments on q7

--- Q8.py
+++ Q8.py
@@ -1,23 +1,41 @@
+# Importing libraries
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 
+# Reading the data from files
 df1 = pd.read_csv('Tutorial22_df.csv')
 df2 = pd.read_csv('Tutorial22_xG_df.csv')
 
 
-high_xg_shots = df2[df2['xg'] > 0.45]
-print(high_xg_shots)
+def addingDistanceCol():
+    # Fetching the x & y cordinates from dataframe df1
+    x = np.array(df1[['xcoord']])
+    y = np.array(df1[['ycoord']])
+
+    # Adding a new column called distance which stores distance between centre and specified coordinates
+    df1['distance'] = ((89 - x) ** 2 + y ** 2) ** 0.5  # pythagoras theorem to calc distance
 
 
-# Define the dependent variable and independent variables
-y = high_xg_shots['xg']
-X = high_xg_shots[['playerid', 'teamid']]
+def StatModel():
+    addingDistanceCol()
 
-# Add an intercept term to the independent variables
-X = sm.add_constant(X)
+    # Merging both the dataframes based on compiled game time
+    df = pd.merge(df1, df2, on=["compiledgametime", 'playerid', 'teamid'])
 
-# Fit the logistic regression model
-model = sm.Logit(y, X).fit()
+    xg_shots = df[df['xg'] > 0.5]
 
-# Print the model summary
-print(model.summary())
+    # Dependent variable and independent variables
+    Y = xg_shots['xg']
+    X = xg_shots[['distance']]
+
+    # Defining the model
+    model = sm.Logit(Y, X).fit()
+    print(model.summary())
+
+    print('\n\nModel: Used Logit Regression to show highest predictor of goal value which is the distance')
+    print('Reason: coefficients, standard errors, p-values, confidence intervals of dependent variable show the '
+          'direction and intensity of the link between each predictor variable and the independent variable.')
+
+
+StatModel()

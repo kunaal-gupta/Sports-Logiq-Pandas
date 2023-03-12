0736a046ae9f2d51a12cf82446026829d2fdb162
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2023-03-12 01:22:16 -0700

    added comments on q5

--- Q4.py
+++ Q4.py
@@ -1,31 +1,50 @@
-import numpy as np
+# Importing pandas  & matplotlib library
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Reading data from csv files to create dataframe
 df1 = pd.read_csv('Tutorial22_df.csv')
 df2 = pd.read_csv('Tutorial22_xG_df.csv')
 
-T1 = df1[df1['teamid'] == 724]
-T2 = df1[df1['teamid'] == 596]
+# Filtering data by team  & eventname
+T1 = df1[(df1['teamid'] == 724) & (df1["eventname"] == "shot")]
+T2 = df1[(df1['teamid'] == 596) & (df1["eventname"] == "shot")]
 
-np.random.seed(19680801)
-
-Data = [(T1[['xcoord', 'ycoord', 'goal']]), (T2[['xcoord', 'ycoord', 'goal']])]
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-marker = ['^', 'o']
-color = ['red', 'blue']
-
-for i in range(len(Data)):
-    x = Data[i].iloc[:, 0:1].values.tolist()
-    y = Data[i].iloc[:, 1:2].values.tolist()
-    z = Data[i].iloc[:, 2:3].values.tolist()
-    ax.scatter(x, y, z, marker=marker[i], c=color[i])
-
-ax.set_xlabel('X coordinate')
-ax.set_ylabel('Y coordinate')
-ax.set_zlabel('Goal')
+# Consolidating individual team data to create a master data
+Data = [[T1[['xcoord']], T1[['ycoord']], T1[['goal']].values.tolist()],
+        [T2[['xcoord']], T2[['ycoord']], T2[['goal']].values.tolist()]]
 
 
-plt.show()
+def scatterPlot(x, y, z, TeamNum=None, **kwargs):
+    """ This function when passed with x, y cordinates along with z value will construct a 2D scatter plot"""
+
+    # Define colors to highlight goaled shots with red
+    colors = ["red" if i[0] == 1 else "green" for i in z]
+
+    # Create scatter plot
+    plt.scatter(x, y, color=colors)
+
+    # Adding labels and scatter plot title
+    plt.xlabel("X-axis label")
+    plt.ylabel("Y-axis label")
+    plt.title("Team {} Scatter Plot".format(TeamNum))
+
+    # Display plot
+    plt.show()
+
+
+def comments():
+    """ Comments on scatter plot"""
+
+    print('Team 596 has made 4 goals as indicated by red dots. The graph design implies that except one '
+          'outlier goal shot, all the shots were present on the positive x coordinates')
+
+    print('Team 724 has made just 1 goal as indicated by red dot. The graph design implies that their shots were '
+          'evently distributed over the field.')
+
+
+# Calling function to draw plot for the individual teams
+
+scatterPlot(x=Data[0][0], y=Data[0][1], z=Data[0][2], TeamNum=724)
+scatterPlot(x=Data[1][0], y=Data[1][1], z=Data[1][2], TeamNum=596)
+comments()
--- Q5.py
+++ Q5.py
@@ -1,32 +1,49 @@
-import numpy as np
+# Importing pandas  & numpy library
 import pandas as pd
+import numpy as np
 
-
+# Reading data from csv files
 df1 = pd.read_csv('Tutorial22_df.csv')
 df2 = pd.read_csv('Tutorial22_xG_df.csv')
 
 
-def parta():
+def part_a():
+    """
+    If the centre of the net that teams shoot at is located at xCoord=89, yCoord=0, create a column for the
+    distance from each shot to this point. What is the distance of the furthest goal scored in the game?
+    """
+
+    # Fetching the x & y cordinates from dataframe df1
     x = np.array(df1[['xcoord']])
     y = np.array(df1[['ycoord']])
 
-    df1['distance'] = ((89-x)**2 + y**2)**0.5
+    # Adding a new column called distance which stores distance between centre and specified coordinates
+    df1['distance'] = ((89 - x) ** 2 + y ** 2) ** 0.5   # pythagoras theorem to calc distance
 
+    # Goal data furthest from centre of the net
     Farthest_Goal_Data = df1.loc[df1['distance'].idxmax()]
+
     print('Distance of the furthest goal scored in the game is {}'.format(Farthest_Goal_Data[['distance']].tolist()[0]))
     return Farthest_Goal_Data
 
-def partb(Farthest_Goal_Data):
 
+def part_b(Farthest_Goal_Data):
+    """
+    What is the expected goals (xg) value of this furthest goal and what do you think contributed to this xg
+    value?
+    """
+    # Connecting both the tables with compilegametime as a foreign key to pull out xg value of the farthest goal shot
     compiledgametime = float(Farthest_Goal_Data[['compiledgametime']].tolist()[0])
 
+    # Storing xg value
     xg_value = (df2[df2['xg'] == compiledgametime])
+
+    # Checking if xg value exists for that goal
     if len(xg_value) == 0:
-        print('No corresponding xg value found')
+        print('No corresponding xg value found!')
     else:
         print('The corresponding xg value for the maximum distance calculated is {}'.format(compiledgametime))
 
 
-Farthest_Goal_Data = parta()
-print()
-partb(Farthest_Goal_Data)
+Farthest_Goal_Data = part_a()
+part_b(Farthest_Goal_Data)
